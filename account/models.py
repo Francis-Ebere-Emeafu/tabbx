ed23187263829b1bde5a1cc5a768888b653cd11a
Emeafu Francis Ebere <freemandigits@gmail.com>
2023-12-25 07:19:30 +0100

    initial commit

---
+++ account/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+from states import STATE_CHOICES
+
+# Create your models here.
+GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Others'),
+]
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=False, null=False)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    surname = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=False, null=False)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
+    birth_date = models.DateField(verbose_name="Date of birth", null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    picture = models.ImageField(upload_to="profile", null=True, blank=True)
+
+    state = models.CharField(max_length=3, choices=STATE_CHOICES, blank=True, null=True)
+    school = models.CharField(max_length=20, blank=True, null=True)
+
+    confirm_email = models.BooleanField(default=False)
+    date_registered = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        if self.middle_name:
+            return "{} {} {}".format(self.first_name, self.middle_name, self.surname)
+        else:
+            return "{} {}".format(self.first_name, self.surname)
+
+
+
+
+class Judge(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
